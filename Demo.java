9d6ad02eaf82c05afe0b6fd54fdebe6e2156683e
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-08-26 11:25:11 +0530

    Method overloading & add gitigonre

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 class Calculator {
-
-    
-
     public int add(int n1, int n2) {
-
         int result = n1 + n2;
         return result;
+    }
+
+    public int add(int n1, int n2, int n3){
+        int result = n1+n2+n3;
+        return result;
+
     }
 }
 
@@ -13,10 +15,12 @@ public class Demo {
     public static void main(String a[]) {
         int num1 = 4;
         int num2 = 5;
+        int num3 = 1;
 
         Calculator calc = new Calculator();
-        int result = calc.add(num1, num2);
-        System.out.println(result);
+        int twoSum = calc.add(num1, num2);
+        int threeSum = calc.add(num1, num2, num3);
+        System.out.println(twoSum);
 
 
     }
